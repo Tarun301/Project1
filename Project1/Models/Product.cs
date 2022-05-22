4bd7da0ea4b9858fe331434baf62712798ad741c
Tarun301 <65336900+Tarun301@users.noreply.github.com>
2022-05-22 19:25:40 +1200

    Add project files.

---
+++ Project1/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Project1.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            Sales = new HashSet<Sale>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+
+        public virtual ICollection<Sale> Sales { get; set; }
+    }
+}
